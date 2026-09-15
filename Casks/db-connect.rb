03e6d0e50444eb6e88bd1308a90db61912a22c41
@shubhesh07 <shubhesh.shukla@truemeds.in>
2026-09-15 12:13:55 +0530

    feat: bump db-connect to v3.2.2
    - version 3.2.2
    - sha256 109d73679d81be0d4fd3448971b0f85e428de2c8b15e7a19fad30d2e24799ea3

--- Casks/db-connect.rb
+++ Casks/db-connect.rb
@@ -1,6 +1,6 @@
 cask "db-connect" do
-  version "3.2.1"
-  sha256 "86ff5273769696b6a8c23d4a4f40841c27bca22e685bd7276a781fd7f9ad3fc7"
+  version "3.2.2"
+  sha256 "109d73679d81be0d4fd3448971b0f85e428de2c8b15e7a19fad30d2e24799ea3"
 
   url "https://github.com/shubhesh07/db-connect/releases/download/v#{version}/DBConnect-macOS-v#{version}.dmg",
       verified: "github.com/shubhesh07/db-connect/"
